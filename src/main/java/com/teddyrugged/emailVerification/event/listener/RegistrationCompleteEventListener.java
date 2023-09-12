8fded9821e0dabf20d897de20f38ce636fc99c7f
teddyrugged <jakpaetuk@gmail.com>
2023-09-12 10:06:19 +0100

    fix folder error

--- src/main/java/com/teddyrugged/emailVerification/event/listener/RegistrationCompleteEventListener.java
+++ src/main/java/com/teddyrugged/emailVerification/event/listener/RegistrationCompleteEventListener.java
@@ -1,10 +1,16 @@
 package com.teddyrugged.emailVerification.event.listener;
 
 import com.teddyrugged.emailVerification.event.RegistrationCompleteEvent;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
 
+@RequiredArgsConstructor
+@Component
 public class RegistrationCompleteEventListener
-        implements ApplicationListener <RegistrationCompleteEvent>{
+        implements ApplicationListener<RegistrationCompleteEvent>{
+
+
     @Override
     public void onApplicationEvent(RegistrationCompleteEvent event) {
 
