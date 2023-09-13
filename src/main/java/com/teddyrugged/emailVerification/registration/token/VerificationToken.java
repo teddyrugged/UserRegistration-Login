84d2250a748f942ac5565ba10c836aa45463d4ed
teddyrugged <jakpaetuk@gmail.com>
2023-09-13 23:03:48 +0100

    verificationToken

--- src/main/java/com/teddyrugged/emailVerification/registration/token/VerificationToken.java
+++ src/main/java/com/teddyrugged/emailVerification/registration/token/VerificationToken.java
@@ -1,2 +1,49 @@
-package com.teddyrugged.emailVerification.registration.token;public class VerificationToken {
+package com.teddyrugged.emailVerification.registration.token;
+
+import com.teddyrugged.emailVerification.user.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Calendar;
+import java.util.Date;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+
+public class VerificationToken {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    private Long id;
+    private String token;
+     private Date expirationTime;
+
+     private static final int EXPIRATION_TIME=15;
+
+     @OneToOne
+     @JoinColumn(name = "user_id")
+     private User user;
+
+    public VerificationToken(String token, User user) {
+        super();
+        this.token = token;
+        this.user = user;
+        this.expirationTime = this.getTokenExpirationTime();
+    }
+    public VerificationToken(String token) {
+        super();
+        this.token = token;
+        this.expirationTime = this.getTokenExpirationTime();
+    }
+
+    public Date getTokenExpirationTime() {
+        Calendar calender = Calendar.getInstance();
+        calender.setTimeInMillis(new Date().getTime());
+        calender.add(Calendar.MINUTE, EXPIRATION_TIME);
+        return new Date(calender.getTime().getTime());
+    }
+
 }
